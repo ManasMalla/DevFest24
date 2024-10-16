af230a16e23bcade6086ab7fc8fc1e7a70ae03bc
Manas Malla <manasmalla.dev@gmail.com>
2024-10-16 08:30:16 +0530

    added code to use m3 packages
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/registration/page.tsx
+++ src/app/registration/page.tsx
@@ -7,6 +7,7 @@ import { collection, doc, getDoc, setDoc } from "firebase/firestore";
 import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { FilledButton, OutlinedTextField } from "material-you-react";
 
 export type FormDataProps = {
   phoneNumber: string;
@@ -95,13 +96,10 @@ export default function page() {
   };
 
   return (
-    <div
-      className="w-full min-h-screen h-full border bg-gray-800 
-        border-gray-700 flex flex-col justify-start items-center"
-    >
+    <div className="w-full min-h-screen h-full flex flex-col justify-start items-center">
       <div className="w-full p-4">
-        <h5 className="mb-5 text-center text-2xl font-semibold text-neutral-200">
-          📃Fill in your details |{" "}
+        <h5 className="mb-5 text-center text-2xl font-semibold">
+          Fill in your details
           {/* <span className="font-mono text-xl">{domain}</span> */}
         </h5>
 
@@ -110,31 +108,15 @@ export default function page() {
           <form className="w-[80%]" onSubmit={handleSubmit}>
             {formFields.map((item, index) => (
               <div key={index} className="mb-5">
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-400"
-                >
-                  {item.label}
-                </label>
-                <input
-                  type={item.type}
-                  id={item.name}
-                  name={item.name}
-                  value={formData[item.name as keyof FormDataProps] || ""}
-                  onChange={(e) => handleChange(e)}
-                  className="bg-gray-700 border border-gray-600 text-white text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-gray-400"
-                  placeholder={item.placeholder}
-                  required
+                <OutlinedTextField
+                  // value={formData[item.name as keyof FormDataProps] || ""}
+                  // onChange={(e) => handleChange(e)}
+                  labelText={item.placeholder}
                 />
               </div>
             ))}
 
-            <button
-              type="submit"
-              className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
-            >
-              Submit
-            </button>
+            <FilledButton>Submit</FilledButton>
           </form>
         </div>
       </div>
